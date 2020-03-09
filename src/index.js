75fcb190a34306da64a38e921ff77b5e3741a95a
KirylZuyeu <kirylzuyeuimh@gmail.com>
2020-03-09 17:34:57 +0300

    init:add a decision of Reverse init task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let arr = [];
+    let item = Math.abs(n).toString();
+    for (let i = 0 ; i < item.length; i++) {
+        arr.push(item[i]);
+    }
+    let number = arr.reverse().join('');
+    return number;
 }
