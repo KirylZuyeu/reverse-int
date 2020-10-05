61da84d36730f44b9b5c2a14822dbe090bd7e135
KirylZuyeu <kirylzuyeuimh@gmail.com>
2020-10-05 18:19:47 +0300

    init: add solution of reverse-init task

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 module.exports = function reverse (n) {
-    let arr = [];
-    let item = Math.abs(n).toString();
-    for (let i = 0 ; i < item.length; i++) {
-        arr.push(item[i]);
-    }
-    let number = arr.reverse().join('');
-    return number;
+    if (n < 0) n = Math.abs(n);
+    return String(n).split('').reverse().join('');
 }
